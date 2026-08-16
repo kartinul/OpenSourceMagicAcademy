bce40064763b9cd6568f2ef1c7671eb5598fc64a
kartinul <kartik.nul@gmail.com>
2026-08-16 18:59:40 +0530

    stuff

---
+++ Assets/Scripts/SetSpriteLayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SetSpriteLayer : MonoBehaviour
+{
+  public int sortingOrder = 5;
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    foreach (SpriteRenderer s in gameObject.GetComponentsInChildren<SpriteRenderer>())
+    {
+      s.sortingOrder = sortingOrder;
+    }
+    foreach (TilemapRenderer s in gameObject.GetComponentsInChildren<TilemapRenderer>())
+    {
+      s.sortingOrder = sortingOrder;
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
