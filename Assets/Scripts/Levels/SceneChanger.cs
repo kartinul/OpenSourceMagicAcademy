2446a6ad5bd5582df5955f7e0cde920029cffb17
Rushil Koul <ruskoul3@gmail.com>
2026-08-16 21:35:18 +0530

    scene workflow

--- Assets/Scripts/Levels/SceneChanger.cs
+++ Assets/Scripts/Levels/SceneChanger.cs
@@ -5,12 +5,10 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
   [SerializeField] private Object sceneToLoad;
-  [SerializeField] private GameObject player;
 
   private void OnTriggerEnter2D(Collider2D other)
   {
-    // Check if the object entering the trigger is the player
-    if (other.gameObject == player)
+    if (other.gameObject.GetComponent<Player>() != null)
     {
       SceneManager.LoadScene(sceneToLoad.name);
     }
